5dc11bd3035110e236458f32bb438f71ac6977b3
kozarezov <kozarezov.dd@gmail.com>
2022-08-09 09:23:34 +0300

    done rewiew

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,47 +1,29 @@
-from posts.models import Comment, Group, Post
 from rest_framework import viewsets
 from rest_framework.generics import get_object_or_404
-from rest_framework.views import PermissionDenied
+from rest_framework.permissions import IsAuthenticated
 
+from posts.models import Group, Post
 from .serializer import CommentSerializer, GroupSerializer, PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied('Вы не являетесь автором поста')
-        super(PostViewSet, self).perform_update(serializer)
-
-    def perform_destroy(self, instance):
-        if instance.author != self.request.user:
-            raise PermissionDenied('Вы не являетесь автором поста')
-        instance.delete()
-
 
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
         post_id = self.kwargs.get('post_id')
         post = get_object_or_404(Post, pk=post_id)
         serializer.save(author=self.request.user, post=post)
-
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied('Вы не являетесь автором поста')
-        super(CommentViewSet, self).perform_update(serializer)
-
-    def perform_destroy(self, instance):
-        if instance.author != self.request.user:
-            raise PermissionDenied('Вы не являетесь автором поста')
-        instance.delete()
 
     def get_queryset(self):
         post_id = self.kwargs.get('post_id')
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -5,9 +5,13 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
+    title = models.CharField('Название группы', max_length=200)
+    slug = models.SlugField('Уникальный url', unique=True)
+    description = models.TextField('Описание группы')
+
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
 
     def __str__(self):
         return self.title
@@ -19,15 +23,20 @@ class Post(models.Model):
         'Дата публикации', auto_now_add=True
     )
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts'
+        User, on_delete=models.CASCADE, related_name='posts',
     )
     image = models.ImageField(
-        upload_to='posts/', null=True, blank=True
+        'Картинка', upload_to='posts/', null=True, blank=True
     )
     group = models.ForeignKey(
-        Group, on_delete=models.CASCADE,
-        related_name="posts", blank=True, null=True
+        Group, on_delete=models.CASCADE, verbose_name='Группа',
+        related_name="posts", blank=True, null=True,
+        help_text='Группа, к которой будет относиться пост',
     )
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
 
     def __str__(self):
         return self.text
@@ -40,7 +49,11 @@ class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name='comments'
     )
-    text = models.TextField()
+    text = models.TextField('Текст комментария', help_text='Текст комментария')
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True
     )
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
